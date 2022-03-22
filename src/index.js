c4d3c9367fc3f451166af6a4cd3890af774f16d7
hyerimYeom <hyerim.yeom@kakao.com>
2022-03-22 23:33:53 +0900

    #5 Subscribing to the store
    - To unsubscribe the change listener, get a return of subscribe method

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import store from './store';
 
+
+//To unsubscribe the change listener, invoke the function returned by subscribe.
+const unsubscribe = store.subscribe(() => { //Rerender for UI if it has changed 
+    console.log("Store changed!", store.getState())
+})
+
 store.dispatch({
     type : "bugAdded",
     payload : {
         discribtion :"Bug1"
     }
 });
+
+// unsubscribe(); // we are not gonna get subscribtion
 
 store.dispatch({
     type : "bugRemoved",
